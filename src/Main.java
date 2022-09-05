dbd3724727282a72b85ddd0dec61e03425e0a83a
Kaixin Chen <kchengit@gmail.com>
2022-09-05 22:31:38 +0200

    Main class

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Welcome! With how many players are you?");
+        String[] playerNames = new String[sc.nextInt()];
+        for (int i = 0; i < playerNames.length; i++) {
+            System.out.printf("What is the name of player %d?\n", i+1);
+            String name = sc.next();
+            playerNames[i] = name;
+        }
+        Game game = new Game(playerNames);
+    }
+
+}
