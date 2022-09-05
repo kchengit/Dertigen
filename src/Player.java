74ba42b8c61d991bab7bd807d40c5cb13b3e0937
Kaixin Chen <kchengit@gmail.com>
2022-09-05 23:34:03 +0200

    Added getSum()

--- src/Player.java
+++ src/Player.java
@@ -36,6 +36,14 @@ public class Player {
         }
     }
 
+    public int getSum() {
+        int sum = 0;
+        for (Dice d : dices) {
+            sum += d.getValue();
+        }
+        return sum;
+    }
+
     public int getSips() {
         return sips;
     }
