faceebe6641b59c8782051fa7467e927031978d6
Kaixin Chen <kchengit@gmail.com>
2022-09-05 22:31:10 +0200

    Dice class

---
+++ src/Dice.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class Dice {
+
+    private int value;
+    private boolean isLocked;
+    Random r = new Random();
+
+    public Dice() {
+        this.value = r.nextInt(6) + 1;
+        this.isLocked = false;
+    }
+
+    public void roll() {
+        this.value = r.nextInt(6) + 1;
+    }
+
+    public void lock() {
+        isLocked = true;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public boolean isLocked() {
+        return isLocked;
+    }
+
+}
